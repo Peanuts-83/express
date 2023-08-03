99dd7e6b2942547fffdcdda0b44550a4ddce163e
Peanuts-83 <tranque@free.fr>
2023-08-03 21:21:36 +0200

    authGuard with jwt

--- guards/auth.guard.js
+++ guards/auth.guard.js
@@ -1,18 +1,25 @@
-const User = require('../models/users')
+const jwt = require('jsonwebtoken')
 
 const authGuardMiddleware = async (req, res, next) => {
-    const authToken = req.header('Authorization')?.split(' ')[1]
-    if (!authToken) {
-        return res.status(401).json({ error: 'Unauthorized' })
-    }
-
-    JsonWebTokenError.verify(authToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ error: 'Invalid token' })
+    try {
+        const token = req.headers.authorization?.split(' ')[1]
+        console.log('token:', token)
+        console.log('req:', req.headers, req.body)
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' })
         }
-        req.userId = decoded.userId
-        next()
-    })
+
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ error: 'Invalid token', err })
+            }
+            req.userId = decoded.userId
+            next()
+        })
+    } catch (err) {
+        console.log(err)
+        res.json('Authentication error', err)
+    }
 }
 
 module.exports = authGuardMiddleware
